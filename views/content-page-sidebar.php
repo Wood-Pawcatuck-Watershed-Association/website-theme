287f6ddea969c7a09328b97dac2c173e9d8a68d7
Jon Waldstein <jonwaldstein@JonWaldteinMini.fios-router.home>
2021-05-01 07:16:08 -0400

    refactor:make page sidebar smaller

--- views/content-page-sidebar.php
+++ views/content-page-sidebar.php
@@ -1,7 +1,7 @@
 <?php if (get_the_content()): ?>
 <div class="container pt-3 pb-3">
     <div class="row">
-    		<div class="col-md-4 d-none d-md-block">
+    		<div class="col-md-3 d-none d-md-block">
     			<div class="page-sidebar">
     				<?php $page_sidebar = carbon_get_the_post_meta('page_sidebar'); ?>
 	    			<?php if (is_active_sidebar($page_sidebar)): ?>
@@ -9,7 +9,7 @@
 			  		<?php endif; ?>
     			</div>
     		</div>
-        <div class="col-md-8">
+        <div class="col-md-9">
             <?php the_content(); ?>
         </div>
     </div>
